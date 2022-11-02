218bb55b4fef41a1b2564ad061c03d3bed23229d
Raphael F. Oliveira <ledrix@hotmail.com>
2022-11-02 01:30:48 -0300

    alterações teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import * as React from "react";
+
+
+
 
 
 export const App = () => {
+
+const [count, setCount] = React.useState(0);
+
+const handleIncrease = () => {
+  setCount(count + 1);
+};
   return (
     <div className="App">
-      teste
+       <button type="button" onClick={handleIncrease}>
+        Increase
+      </button>
+
+      <div>{count}</div>
     </div>
   );
-}
+};
 
